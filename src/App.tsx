e6825d7ca10102be7347c59b879c3e006656505c
Vicente Silva <ildealef@outlook.com>
2025-06-26 10:39:36 -0300

    TaskModel.ts and TaskStateModel.ts created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-// import { Home } from './pages/Home';
-import { NotFound } from './pages/NotFound';
-import { AboutPomodoro } from './pages/AboutPomodoro';
+import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  return <NotFound />;
+  return <Home />;
 }
