35f64b34bc7c46ed0ce33feaba05d26de70778cf
Vicente <ildealef@outlook.com>
2025-12-15 21:12:20 -0300

    Refactor MainForm to integrate getNextCycle and getNextCycleType functions for improved task cycle management

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,31 +5,36 @@ import { DefaultInput } from '../DefautInput';
 import { useRef } from 'react';
 import type { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../context/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 
 export function MainForm() {
-  const { setState } = useTaskContext();
-  const taskNameInput = useRef<HTMLInputElement>(null);
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null); //forma não controlada de capturar oi valor do input
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    if (!taskNameInput.current) return;
+    if (!taskNameInput.current) return; //verifica se a ref está atribuída onde se for nula, sai da função
 
-    const taskName = taskNameInput.current.value.trim();
+    const taskName = taskNameInput.current.value.trim(); // obtém o valor do input usando a ref e trata espaços em branco
 
     if (!taskName) {
-      alert('Digite o nome da tarefa');
+      alert('Digite o nome da tarefa'); // validação simples para garantir que o nome da tarefa não esteja vazio
       return;
     }
 
     const newTask: TaskModel = {
-      id: Date.now().toString(),
+      id: Date.now().toString(), // Gera um ID único baseado no timestamp atual em string
       name: taskName,
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: 'workTime',
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
@@ -37,9 +42,9 @@ export function MainForm() {
     setState(prevState => {
       return {
         ...prevState,
-        config: {...prevState.config},
+        config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1,
+        currentCycle: nextCycle,
         secondsRemaining,
         formattedSecondsRemaining: '00:00',
         tasks: [...prevState.tasks, newTask],
@@ -55,7 +60,7 @@ export function MainForm() {
           type='text'
           labelText='Task:'
           placeholder='Insira uma tarefa'
-          ref={taskNameInput}
+          ref={taskNameInput} // utiliza a ref para capturar o valor do input
         />
       </div>
 
